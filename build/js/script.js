977f2b2f6c66b3c9c9a344b40cbeded979a30393
Quakush <quakush@mail.ru>
2020-09-22 16:41:58 +0700

    finished

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,80 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+var link = document.querySelector('.user-navigation__link');
+var popup = document.querySelector('.modal__login');
+var closeLogin = document.querySelector('.modal__close--login');
+var login = popup.querySelector('[name=login]');
+var password = popup.querySelector('[name=passord]');
+var form = popup.querySelector('.form-login');
+var mapLink = document.querySelector('.contacts__btn');
+var mapPopup = document.querySelector('.modal__map');
+var mapClose = mapPopup.querySelector('.modal__close--map');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+var isStorageSupport = true;
+var storage = '';
+console.log(mapLink);
+try {
+  storage = localStorage.getItem('login');
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.add('modal__show');
+
+  if (storage) {
+    login.value = storage;
+    password.focus();
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    login.focus();
   }
 });
+
+closeLogin.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.remove('modal__show');
+  popup.classList.remove('modal__error');
+});
+
+form.addEventListener('submit', function (evt) {
+  if (!login.value || !password.value) {
+    evt.preventDefault();
+    popup.classList.remove('modal__error');
+    popup.classList.add('modal__error');
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem('login', login.value);
+    }
+  }
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains('modal__show')) {
+      popup.classList.remove('modal__show');
+      popup.classList.remove('modal__error');
+    }
+  }
+});
+
+mapLink.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add('modal__show');
+});
+
+mapClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove('modal__show');
+});
+
+window.addEventListener('keydown', function (evt) {
+  evt.preventDefault();
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains('modal__show')) {
+      mapPopup.classList.remove('modal__show');
+    }
+  }
+});
+
